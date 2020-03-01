82464ec76f11a6a3f5241a1cab307383bf7ca723
魏巍 <duanyu@baofoo.com>
2020-03-01 22:44:58 +0800

    optimization

---
+++ shyin/nginx/2/lua-install.sh
@@ -0,0 +1,12 @@
+## 安装lua
+wget http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
+tar -xvf LuaJIT-2.1.0-beta3.tar.gz && cd LuaJIT-2.1.0-beta3
+make && make install
+# make && make install PREFIX=/usr/local/luajit
+
+ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
+
+echo "export LUAJIT_LIB=/usr/local/lib" >> /etc/profile
+echo "export LUAJIT_INC=/usr/local/include/luajit-2.1" >> /etc/profile
+echo "export LD_LIBRARY_PATH=/usr/local/luajit/lib:$LD_LIBRARY_PATH" >> /etc/profile
+source /etc/profile
--- shyin/nginx/2/nginx-install.sh
+++ shyin/nginx/2/nginx-install.sh
@@ -1,7 +1,7 @@
 #安装目录 
-INSTALL_DIR=/usr/local/ 
-SRC_DIR=/usr/local/software 
-NGINX="nginx-1.13.7" 
+INSTALL_DIR=/usr/local/
+SRC_DIR=/usr/local/software
+NGINX="nginx-1.13.7"
 
 mkdir -p ${INSTALL_DIR}
 mkdir -p ${SRC_DIR}
@@ -10,8 +10,12 @@ cd /${SRC_DIR}/
 
 #解压
 tar xzf ${SRC_DIR}/${NGINX}.tar.gz -C /${SRC_DIR}/
+#解压lua
+tar -xvf v0.3.0.tar.gz
+tar -xvf v0.10.11.tar.gz
 
-cd ${SRC_DIR}
+cd ${SRC_DIR}/${NGINX}
+# INSTALL_DIR=/usr/local/test/
 
 #参数
 NGINXFEATURES="--prefix=${INSTALL_DIR}nginx \
@@ -21,8 +25,10 @@ NGINXFEATURES="--prefix=${INSTALL_DIR}nginx \
 --with-http_ssl_module \
 --with-http_v2_module \
 --with-http_gzip_static_module \
---with-http_sub_module"
-
+--with-http_sub_module \
+--with-http_limit_conn_zone \
+--add-module=/usr/local/software/ngx_devel_kit-0.3.0 \
+--add-module=/usr/local/software/lua-nginx-module-0.10.11 "
 
 #添加用户
 groupadd -r www
