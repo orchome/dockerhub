81364e56c75fd0b3f97f04de425a362b7090b4f1
魏巍 <duanyu@baofoo.com>
2020-03-02 19:03:59 +0800

    o

--- shyin/nginx/2/lua-install.sh
+++ shyin/nginx/2/lua-install.sh
@@ -1,4 +1,7 @@
 ## 安装lua
+SRC_DIR=/usr/local/software
+cd /${SRC_DIR}/
+
 wget http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
 tar -xvf LuaJIT-2.1.0-beta3.tar.gz && cd LuaJIT-2.1.0-beta3
 make && make install
