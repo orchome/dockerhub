ee38d9d17a73fb74c81386704a73335a58a6618f
魏巍 <duanyu@baofoo.com>
2020-03-04 21:13:13 +0800

    test

--- shyin/cmd.sh
+++ shyin/cmd.sh
@@ -1,6 +1,20 @@
-V`P]6E<O&e[Y
 kubectl exec -ti -n nginx `kubectl get pods -n nginx|grep Running|grep php-app|awk '{print $1}'` /bin/bash
 kubectl exec -ti -n nginx `kubectl get pods -n nginx|grep Running|grep php-test|awk '{print $1}'` /bin/bash
-wget https://www.php.net/distributions/php-7.2.28.tar.gz
 
 ## kubectl create secret docker-registry mysecret --docker-server=uhub.service.ucloud.cn --docker-username=263230286@qq.com --docker-password=shyinwork -n jenkins
+
+## 广东
+kubectl label nodes 10.13.119.72  area=A
+kubectl label nodes 10.13.137.159 area=A
+kubectl label nodes 10.13.164.42  area=B
+kubectl label nodes 10.13.35.52   area=B
+kubectl label nodes 10.13.58.179  area=C
+kubectl label nodes 10.13.85.238  area=C
+
+## 香港
+kubectl label nodes 10.8.16.170 area=A
+kubectl label nodes 10.8.179.50 area=A
+kubectl label nodes 10.8.23.154 area=B
+kubectl label nodes 10.8.60.68  area=B
+kubectl label nodes 10.8.89.175 area=C
+kubectl label nodes 10.8.90.105 area=C
