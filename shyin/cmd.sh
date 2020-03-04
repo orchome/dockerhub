6d2f84d1a77cd3f6970231b2c0dc69cef21dc108
魏巍 <duanyu@baofoo.com>
2020-03-04 18:56:22 +0800

    optimization

--- shyin/cmd.sh
+++ shyin/cmd.sh
@@ -2,3 +2,5 @@ V`P]6E<O&e[Y
 kubectl exec -ti -n nginx `kubectl get pods -n nginx|grep Running|grep php-app|awk '{print $1}'` /bin/bash
 kubectl exec -ti -n nginx `kubectl get pods -n nginx|grep Running|grep php-test|awk '{print $1}'` /bin/bash
 wget https://www.php.net/distributions/php-7.2.28.tar.gz
+
+## kubectl create secret docker-registry mysecret --docker-server=uhub.service.ucloud.cn --docker-username=263230286@qq.com --docker-password=shyinwork -n jenkins
